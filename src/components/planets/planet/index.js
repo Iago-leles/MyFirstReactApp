babce8e3089fef7782b0d316d78f3135fdbe644d
Iago-leles <iago.leles@gmail.com>
2022-11-25 16:56:35 -0300

    dividindo e reutilizando coomponents

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,12 +1,12 @@
 import React, {Fragment} from "react";
-import './style.css'
+import GrayImg from '../../shared/gray-img'
 
 const Planet = () => {
     return(
         <div>
             <h4>Mercúrio</h4>
             <p>Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres.</p>
-            <img class='gray-img' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"></img>
+            <GrayImg/>
         </div>
     )
 }
